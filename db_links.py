89d162042eaa5be522259680dfa941fad7a5155e
lippings <samuel.lipping@tuni.fi>
2019-05-03 12:38:51 +0300

    cleaner

--- db_links.py
+++ db_links.py
@@ -14,49 +14,23 @@ from __future__ import print_function
 import dropbox
 from time import time
 import numpy as np
-
-
-def printProgressBar (iteration, total, prefix='Progress:', additional=[], suffix='Completed', decimals=1, length=50,
-                      fill='█'):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    string = '\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix)
-    string = "\t".join([string]+additional)
-    print(string, end = '')
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
+from tqdm import tqdm
 
 
 def main():
     colDelim = ","
     rowDelim = "\n"
-	
-	client_id = "<Your client id here>"
+
+    client_id = "<Your client id here>"
     audio_folder = "<Location folder of the audio files on Dropbox>"
-	csv_destination = "<Destination folder of the csv file on Dropbox>"
-	
-    
+    csv_destination = "<Destination folder of the csv file on Dropbox>"
+
     dbx = dropbox.Dropbox(client_id)
     data = [["audioUrl", "audioType"]]
     contents = dbx.files_list_folder(audio_folder).entries
-    N = len(contents)
-    cnt = 0
     start_time = time()
     times = []
-    for entry in contents:
+    for entry in tqdm(contents):
         lap_start = time()
         name = entry.name
         # Continue, if entry has an extension, i.e. it is a file not a folder
@@ -78,10 +52,7 @@ def main():
             data.append([url, audio_type])
 
             times.append(time()-lap_start) 
-            cnt += 1
-            printProgressBar(cnt, N, suffix="Links created", additional=["Creating link for %s" % (name)])
-            # print(f"Time for one file: {time()-lap_start}\tProgress: {cnt}/{N}")
-            
+
     print("Total time elapsed for share links: {}, mean for one: {}".format(
         time()-start_time,
         np.mean(times)
